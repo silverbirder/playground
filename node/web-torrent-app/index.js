86519f78249e67b8659a26828f7b0cd15e0a433b
shibamoto <silverbirder@gmail.com>
2021-12-05 12:23:22 +0900

    🙂 inject env to code

--- node/web-torrent-app/index.js
+++ node/web-torrent-app/index.js
@@ -1,11 +1,14 @@
+require('dotenv').config()
+
 const WebTorrent = require('webtorrent')
 
 const client = new WebTorrent()
 
-const magnetURI = 'xxx';
+const magnetURI = process.env.MAGNET_URI;
 
 client.add(magnetURI, { path: './output' }, function (torrent) {
   torrent.on('done', function () {
     console.log('torrent download finished')
   })
+  
 })
