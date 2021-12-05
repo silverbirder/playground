1c498485f00fbab79a58491bff4c07c109a40b18
shibamoto <silverbirder@gmail.com>
2021-12-05 12:38:15 +0900

    😕 Watch debug log

--- node/web-torrent-app/index.js
+++ node/web-torrent-app/index.js
@@ -6,9 +6,43 @@ const client = new WebTorrent()
 
 const magnetURI = process.env.MAGNET_URI;
 
-client.add(magnetURI, { path: './output' }, function (torrent) {
-  torrent.on('done', function () {
-    console.log('torrent download finished')
-  })
-  
-})
+client.add(magnetURI, { path: './output' }, (torrent) => {
+    torrent
+        .on('done', () => {
+            console.log('torrent done event');
+        })
+        .on('infoHash', () => {
+            console.log('torrent infoHash event');
+        })
+        .on('metadata', () => {
+            console.log('torrent metadata event');
+        })
+        .on('ready', () => {
+            console.log('torrent ready event');
+        }).
+        on('warning', (err) => {
+            console.log('torrent warning event');
+            console.log(err);
+        }).
+        on('error', (err) => {
+            console.log('torrent error event');
+            console.log(err);
+        }).
+        on('download', (bytes) => {
+            console.log('torrent download event');
+            console.log(bytes);
+        }).
+        on('upload', (bytes) => {
+            console.log('torrent upload event');
+            console.log(bytes);
+        }).
+        on('wire', (wire, addr) => {
+            console.log('torrent wire event');
+            // console.log(wire);
+            console.log(addr);
+        }).
+        on('noPeers', (announceType) => {
+            console.log('torrent noPeers event');
+            console.log(announceType);
+        })
+});
