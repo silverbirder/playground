1c47ea0fa8a1b26da9508f0ff1a67644a9d150f6
shibamoto <silverbirder@gmail.com>
2021-12-05 20:53:16 +0900

    Refactoring

--- node/web-torrent-app/index.js
+++ node/web-torrent-app/index.js
@@ -19,29 +19,29 @@ client.add(magnetURI, { path: './output' }, (torrent) => {
         })
         .on('ready', () => {
             console.log('torrent ready event');
-        }).
-        on('warning', (err) => {
+        })
+        .on('warning', (err) => {
             console.log('torrent warning event');
             console.log(err);
-        }).
-        on('error', (err) => {
+        })
+        .on('error', (err) => {
             console.log('torrent error event');
             console.log(err);
-        }).
-        on('download', (bytes) => {
+        })
+        .on('download', (bytes) => {
             console.log('torrent download event');
             console.log(bytes);
-        }).
-        on('upload', (bytes) => {
+        })
+        .on('upload', (bytes) => {
             console.log('torrent upload event');
             console.log(bytes);
-        }).
-        on('wire', (wire, addr) => {
+        })
+        .on('wire', (wire, addr) => {
             console.log('torrent wire event');
-            // console.log(wire);
+            console.log(wire);
             console.log(addr);
-        }).
-        on('noPeers', (announceType) => {
+        })
+        .on('noPeers', (announceType) => {
             console.log('torrent noPeers event');
             console.log(announceType);
         })
