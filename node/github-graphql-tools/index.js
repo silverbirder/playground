33887a85ff678c6fa12756f9094cc8d0ac6708a2
shibamoto <silverbirder@gmail.com>
2021-12-07 18:41:37 +0900

    😎 update github graphql

--- node/github-graphql-tools/index.js
+++ node/github-graphql-tools/index.js
@@ -5,13 +5,24 @@ var github = new GithubGraphQLApi({
 })
 github.query(`
 {
-    user(login: "Silver-birder") {
-        repositories(first: 20, orderBy: {field: UPDATED_AT, direction: DESC}) {
-            nodes {
-                name
+  user(login: "Silver-birder") {
+    repositories(first: 20, orderBy: {field: UPDATED_AT, direction: DESC}, isFork: false) {
+      nodes {
+        object(expression: "main:") {
+          id
+          ... on Tree {
+            entries {
+              name
+              path
+              extension
+              mode
+              type
             }
+          }
         }
+      }
     }
+  }
 }
 `, null, (res, err) => {
   console.log(JSON.stringify(res, null, 2))
