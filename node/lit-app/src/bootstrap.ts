9b07ccdab4bb492d0592cd3bd38e11146b8598a8
silverbirder <silverbirder@gmail.com>
2021-12-10 10:02:13 +0000

    Add sample code

--- node/lit-app/src/bootstrap.ts
+++ node/lit-app/src/bootstrap.ts
@@ -1,3 +1,19 @@
 export { MyElement } from './my-element';
-import confetti from 'canvas-confetti';
-confetti();
+// import confetti from 'canvas-confetti';
+// confetti();
+
+//@ts-ignore
+// import * as markdown from 'markdown-wasm/dist/markdown.es.js';
+
+// markdown.ready.then(async () => {
+//     const res = await (await fetch('./sample.md')).text();
+//     document.body.innerHTML = markdown.parse(res);
+// })
+
+import monaco from 'monaco-editor';
+
+//@ts-ignore
+monaco.editor.create(document.getElementById('container'), {
+	value: "function hello() {\n\talert('Hello world!');\n}",
+	language: 'javascript'
+});
