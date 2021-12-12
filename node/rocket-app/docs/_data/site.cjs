6c654c8ccea8077f7f6ff0672e900f3dd45322a7
shibamoto <silverbirder@gmail.com>
2021-12-12 20:57:52 +0900

    Add sample portfolio page

---
+++ node/rocket-app/docs/_data/site.cjs
@@ -0,0 +1,25 @@
+module.exports = function () {
+  return {
+    dir: 'ltr',
+    lang: 'en',
+    name: 'silverbirder',
+    description: 'Rocket is the way to build fast static websites with a sprinkle of JavaScript',
+    socialLinks: [
+      {
+        name: 'GitHub',
+        url: 'https://github.com/Silver-birder/Silver-birder.github.io',
+      },
+      {
+        name: 'Twitter',
+        url: 'https://twitter.com/silver_birder',
+      }
+    ],
+    gitSiteUrl: 'https://github.com/Silver-birder/Silver-birder.github.io',
+    gitBranch: 'master',
+    helpUrl: 'https://github.com/Silver-birder/Silver-birder.github.io/issues',
+    logoAlt: 'Rocket Logo',
+    iconColorMaskIcon: '#3f93ce',
+    iconColorMsapplicationTileColor: '#1d3557',
+    iconColorThemeColor: '#1d3557',
+  };
+};
---
+++ node/rocket-app/docs/watch/contents/sample2.md
@@ -0,0 +1,2 @@
+# Content2
+hey
