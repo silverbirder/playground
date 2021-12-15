a76b34fa2714a7f9e0e1833dc0aa3acaada6f243
shibamoto <silverbirder@gmail.com>
2021-12-15 14:43:53 +0900

    🙂 Upload to google drive from rust

---
+++ rust/google-drive-app/src/main.rs
@@ -0,0 +1,36 @@
+extern crate dotenv;
+extern crate google_drive3;
+extern crate yup_oauth2;
+
+use dotenv::dotenv;
+use google_drive3::oauth2 as oauth;
+use google_drive3::{api::File, hyper, hyper_rustls, DriveHub};
+use std::{env, fs};
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+    let service_account_key = oauth::read_service_account_key(&"key.json".to_string())
+        .await
+        .expect("client secret couldn't be read.");
+    let authenticator = oauth::ServiceAccountAuthenticator::builder(service_account_key)
+        .build()
+        .await
+        .expect("failed to create authenticator");
+    let client = hyper::Client::builder().build(hyper_rustls::HttpsConnector::with_native_roots());
+
+    let hub = DriveHub::new(client, authenticator);
+    let mut req = File::default();
+    let google_drive_id = env::var("GOOGLE_DRIVE_ID").expect("GOOGLE_DRIVE_ID must be set");
+    req.parents = Some(vec![google_drive_id]);
+    req.name = Some("main.rs".to_string());
+    hub.files()
+        .create(req)
+        .add_scope("https://www.googleapis.com/auth/drive.file")
+        .upload(
+            fs::File::open("./src/main.rs").unwrap(),
+            "application/octet-stream".parse().unwrap(),
+        )
+        .await
+        .expect("failed to upload google drive");
+}
