aade3a224101972392efdcd6440f32456de447ee
shibamoto <silverbirder@gmail.com>
2021-12-17 09:10:20 +0900

    🙂 Add code

--- node/web-torrent-app/src/app.js
+++ node/web-torrent-app/src/app.js
@@ -2,11 +2,13 @@ const WebTorrent = require('webtorrent')
 const fs = require('fs');
 const { drive, auth } = require('@googleapis/drive');
 const { JWT } = auth;
-const keys = require('./jwt.keys.json');
+
+const client = new WebTorrent();
+
 const jwtClient = new JWT(
-    keys.client_email,
+    process.env.SERVICE_ACCOUNT_CLIENT_EMAIL,
     null,
-    keys.private_key,
+    process.env.SERVICE_ACCOUNT_PRIVATE_KEY,
     ['https://www.googleapis.com/auth/drive.file'],
 );
 
@@ -15,71 +17,65 @@ const app = express();
 
 app.use(express.json());
 
+const _log = (message) => {
+    if (process.env.NODE_ENV !== 'production') {
+        console.log(message);
+    }
+}
+
+const _err_log = (error) => {
+    console.error(error);
+}
+
 app.get('/', async (req, res) => {
-    const client = new WebTorrent();
     const magnetURI = req.query.magnet_uri;
     if (!magnetURI) {
         res.status(200).send('not found magnetURI');
         return;
     }
     await jwtClient.authorize();
-    client.add(magnetURI, { path: process.env.OUTPUT_PATH || './output' }, (torrent) => {
-        torrent
-            .on('done', async () => {
-                console.log('torrent done event');
-                const driveService = drive({
-                    version: 'v3',
-                    auth: jwtClient
-                });
-                torrent.files.forEach(async (file) => {
-                    await driveService.files.create({
-                        resource: {
-                            name: file.name,
-                            parents: [process.env.GOOGLE_DRIVE_ID]
-                        },
-                        media: {
-                            body: fs.createReadStream(file.path)
-                        },
+    client
+        .on('error', (err) => {
+            _log('catch client error');
+            _err_log(err);
+        })
+        .add(magnetURI, { path: process.env.OUTPUT_PATH, destroyStoreOnDestroy: true }, (torrent) => {
+            torrent
+                .on('done', async () => {
+                    console.log('torrent done event');
+                    const driveService = drive({
+                        version: 'v3',
+                        auth: jwtClient
                     });
-                });
-                client.destroy();
-                console.log('client destroy');
-            })
-            .on('infoHash', () => {
-                // console.log('torrent infoHash event');
-            })
-            .on('metadata', () => {
-                // console.log('torrent metadata event');
-            })
-            .on('ready', () => {
-                // console.log('torrent ready event');
-            })
-            .on('warning', (err) => {
-                console.log('torrent warning event');
-                console.log(err);
-            })
-            .on('error', (err) => {
-                console.log('torrent error event');
-                console.log(err);
-                client.destroy();
-            })
-            .on('download', (bytes) => {
-                // console.log(`【torrent download event】progress:${torrent.progress},downloaded:${torrent.downloaded},downloadSpeed:${torrent.downloadSpeed}(numPeers:${torrent.numPeers})`);
-            })
-            .on('upload', (bytes) => {
-                // console.log('torrent upload event');
-                // console.log(bytes);
-            })
-            .on('wire', (wire, addr) => {
-                // console.log('torrent wire event');
-                // console.log(wire);
-                // console.log(addr);
-            })
-            .on('noPeers', (announceType) => {
-                // console.log('torrent noPeers event');
-                // console.log(announceType);
-            })
-    });
+                    console.log('file uploading...');
+                    await Promise.all(torrent.files.map( async (file) => {
+                        return driveService.files.create({
+                            resource: {
+                                name: file.name,
+                                parents: [process.env.GOOGLE_DRIVE_ID]
+                            },
+                            media: {
+                                body: fs.createReadStream(file.path)
+                            },
+                        });
+                    }));
+                    console.log('file uploaded');
+                    torrent.destroy();
+                    console.log('torrent destroy');
+                })
+                .on('warning', (err) => {
+                    _log('torrent warning event');
+                    _err_log(err);
+                })
+                .on('error', (err) => {
+                    _log('torrent error event');
+                    _err_log(err);
+                    torrent.destroy();
+                })
+                .on('download', (bytes) => {
+                    _log(`【torrent download event】progress:${torrent.progress},downloaded:${torrent.downloaded},downloadSpeed:${torrent.downloadSpeed}(numPeers:${torrent.numPeers})`);
+                })
+        });
     res.status(200).send('recept magnetURI');
 });
 
