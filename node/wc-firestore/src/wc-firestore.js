82a3e3fd5ad585578c8df28ca2490050ad61cd4e
shibamoto <silverbirder@gmail.com>
2022-01-13 21:22:45 +0900

    Add wc firestore

--- node/wc-firestore/src/wc-firestore.js
+++ node/wc-firestore/src/wc-firestore.js
@@ -1,18 +1,3 @@
 import { WcFirestore } from './WcFirestore.js';
 
 customElements.define('wc-firestore', WcFirestore);
-
-import { initializeApp } from "firebase/app"
-import { getFirestore, collection, getDocs } from "firebase/firestore"
-
-(async () => {
-    const firebaseApp = initializeApp({
-        apiKey: process.env.FIREBASE_API_KEY,
-        projectId: process.env.FIREBASE_PROJECT_ID,
-    });
-    const db = getFirestore();
-    const querySnapshot = await getDocs(collection(db, "collection"));
-    querySnapshot.forEach((doc) => {
-        console.log(doc.data());
-    });
-})();
