a7d6cd06ca8346ecc6c09fff8530ba1c3a621228
shibamoto <silverbirder@gmail.com>
2022-03-12 18:24:43 +0900

    Add sample code

---
+++ node/my-oembed-app/react-app/src/App.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <o-embed
+          src="https://twitter.com/silver_birder/status/1176375655237214209"
+          proxy="https://silverbirder-cors-anywhere.herokuapp.com"
+          height="900px"
+        ></o-embed>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
