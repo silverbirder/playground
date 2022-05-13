428f0a5844f2a315d2ba25402b5b6a2897b3b8bd
shibamoto <silverbirder@gmail.com>
2022-05-13 23:52:31 +0900

    :construction: uum

--- node/re-rendering-sample/src/App.js
+++ node/re-rendering-sample/src/App.js
@@ -1,74 +1,73 @@
 import React, { useState, memo, useCallback } from "react";
 
-const isItemEqual = (prevProps, nextProps) => {
-  return JSON.stringify(prevProps.item) === JSON.stringify(nextProps.item);
+const isEqual = (prevProps, nextProps) => {
+  return JSON.stringify(prevProps) === JSON.stringify(nextProps);
 };
 
 const InputCell = memo(({ text, onChange }) => {
   console.log(`render InputCell (text:${text})`);
   return <input type="text" value={text} onChange={onChange}></input>;
-});
+}, isEqual);
 
-const Item = memo(({ item, updateItems }) => {
+const Item = memo(({ item, setItem }) => {
   console.log(`render Item (id:${item.id})`, { item });
   const onChange = useCallback(
     (e) => {
       const newItem = { ...item, text: e.target.value };
-      updateItems(newItem);
+      setItem(newItem);
     },
-    [item, updateItems]
+    [item, setItem]
   );
   return (
-    <li>
-      {item.id}:
-      <InputCell text={item.text} onChange={onChange} />
-    </li>
+    <>
+      <li>
+        {item.id}:
+        <InputCell text={item.text} onChange={onChange} />
+      </li>
+    </>
   );
-}, isItemEqual);
+}, isEqual);
 
-const Items = memo(({ items, updateItems }) => {
+const Items = memo(({ items, setItems }) => {
   console.log("render Items", { items });
-  return (
-    <ul>
-      {items.map((item) => {
-        return <Item item={item} key={item.id} updateItems={updateItems} />;
-      })}
-    </ul>
+  const setItem = useCallback(
+    (item) => {
+      const newItems = [...items];
+      newItems[item.id - 1] = item;
+      setItems(newItems);
+    },
+    [items, setItems]
   );
-});
+  return (
+    <>
+      <ul>
+        {items.map((item) => (
+          <Item item={item} key={item.id} setItem={setItem} />
+        ))}
+      </ul>
+    </>
+  );
+}, isEqual);
 
-function App() {
+const App = React.memo(() => {
   console.log("render App");
   const [cnt, setCnt] = useState(1);
   const [items, setItems] = useState([{ id: Number(cnt), text: "" }]);
-  const onClick = () => {
+  const onClick = useCallback(() => {
     setCnt(cnt + 1);
     const newItems = [...items];
     newItems.push({ id: newItems.length + 1, text: "" });
     setItems(newItems);
-  };
-  const updateItems = useCallback(
-    (propItem) => {
-      const newItems = items.map((item) => {
-        if (item.id === propItem.id) {
-          return propItem;
-        } else {
-          return item;
-        }
-      });
-      setItems(newItems);
-    },
-    [items]
-  );
+  }, [cnt, items]);
   return (
     <>
       {cnt}
       <br />
       <button onClick={onClick}>button</button>
       <br />
-      <Items items={items} updateItems={updateItems} />
+      <Items items={items} setItems={setItems} />
     </>
   );
-}
+});
 
 export default App;
