390914edc39fe1b3725555e1eb1d36891a40c242
shibamoto <silverbirder@gmail.com>
2022-06-12 10:53:19 +0900

    :recycle: Refactor test code

--- node/react-testing-training/src/components/CounterButton.test.tsx
+++ node/react-testing-training/src/components/CounterButton.test.tsx
@@ -1,6 +1,10 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import { CounterButton } from "./CounterButton";
-it("a", () => {
-  render(<CounterButton />);
-  fireEvent.click(screen.getByText("count"));
+describe("CounterButton", () => {
+  it("countボタンを押したら、表示されている数字が1増える", () => {
+    render(<CounterButton />);
+    expect(screen.getByText("0")).toBeInTheDocument();
+    fireEvent.click(screen.getByText("count"));
+    expect(screen.getByText("1")).toBeInTheDocument();
+  });
 });
--- node/react-testing-training/src/components/hooks/useCounter.test.ts
+++ node/react-testing-training/src/components/hooks/useCounter.test.ts
@@ -2,7 +2,7 @@ import { renderHook, act } from "@testing-library/react-hooks";
 import { useCounter } from "./useCounter";
 
 describe("useCounter", () => {
-  it("incrementしたら、countが1増える", () => {
+  it("incrementを実行したら、countが1増える", () => {
     const { result } = renderHook(() => useCounter());
     expect(result.current.count).toBe(0);
     act(() => {
