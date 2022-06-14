bec3907a084ceb4fb7476a3ace9649699853c928
shibamoto <silverbirder@gmail.com>
2022-06-14 22:57:13 +0900

    :recycle: Refactor

--- node/re-rendering-sample/src/App.js
+++ node/re-rendering-sample/src/App.js
@@ -3,13 +3,14 @@ import React, { useState, memo, useCallback } from "react";
 const InputCell = memo(({ text, onChange }) => {
   console.log(`render InputCell (text:${text})`);
   return <input type="text" value={text} onChange={onChange}></input>;
-});
+}, (prev, next) => JSON.stringify(prev.text) === JSON.stringify(next.text) && prev.onChange === next.onChange);
 
 const Item = memo(({ item, setItem }) => {
   console.log(`render Item (id:${item.id})`, { item });
   const onChange = useCallback(
     (e) => {
-      const newItem = { ...item, text: e.target.value };
+      const newItem = {...item};
+      newItem.text = e.target.value;
       setItem(newItem);
     },
     [item, setItem]
@@ -22,14 +23,14 @@ const Item = memo(({ item, setItem }) => {
       </li>
     </>
   );
-});
+}, (prev, next) => JSON.stringify(prev.item) === JSON.stringify(next.item) && prev.setItem === next.setItem);
 
 const Items = memo(({ items, setItems }) => {
   console.log("render Items", { items });
   const setItem = useCallback(
     (item) => {
-      const newItems = [...items];
-      newItems[item.id - 1] = item;
+      const newItems = items.map((item) => ({...item}));
+      newItems[item.id - 1] = {...item};
       setItems(newItems);
     },
     [items, setItems]
@@ -43,16 +44,16 @@ const Items = memo(({ items, setItems }) => {
       </ul>
     </>
   );
-});
+},  (prev, next) => JSON.stringify(prev.items) === JSON.stringify(next.items));
 
-const App = React.memo(() => {
+const App = () => {
   console.log("render App");
   const [cnt, setCnt] = useState(1);
   const [items, setItems] = useState([{ id: Number(cnt), text: "" }]);
   const onClick = useCallback(() => {
     setCnt(cnt + 1);
-    const newItems = [...items];
-    newItems.push({ id: newItems.length + 1, text: "" });
+    const newItems = items.map((item) => ({...item}));
+    newItems.push({ id: items.length + 1, text: "" });
     setItems(newItems);
   }, [cnt, items]);
   return (
@@ -64,6 +65,6 @@ const App = React.memo(() => {
       <Items items={items} setItems={setItems} />
     </>
   );
-});
+};
 
 export default App;
