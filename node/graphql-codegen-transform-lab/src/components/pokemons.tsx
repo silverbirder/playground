d5020b8ac7dd4a34c8c82feadb9036615a67c864
shibamoto <silverbirder@gmail.com>
2022-07-27 21:13:54 +0900

    :recycle: Refactor

--- node/graphql-codegen-transform-lab/src/components/pokemons.tsx
+++ node/graphql-codegen-transform-lab/src/components/pokemons.tsx
@@ -1,33 +1,10 @@
-import { useQuery } from "urql";
-import {
-  Pokemon,
-  Pokemons,
-  PokemonsQuery,
-} from "../graphql-client/generated/graphql";
-
-const transform = (
-  pokemons: Array<Pokemon | null> | null | undefined
-): Pokemon[] => {
-  if (!pokemons) {
-    return [];
-  }
-  const newPokemons: Pokemon[] = [];
-  pokemons.forEach((pokemon: Pokemon | null) => {
-    if (pokemon === null) {
-      return;
-    }
-    newPokemons.push(pokemon);
-  });
-  return newPokemons;
-};
+import { Pokemon } from "../graphql-client/generated/graphql";
+import { usePokemonsQuery } from "./usePokemonsQuery";
 
 const PokemonsComponent = () => {
-  const result = useQuery<PokemonsQuery>({ query: Pokemons });
-  const [query] = result;
-  const { data, fetching, error } = query;
+  const { pokemons, fetching, error } = usePokemonsQuery();
   if (fetching) return <p>Loading...</p>;
   if (error) return <p>Oh no... {error.message}</p>;
-  const pokemons = transform(data?.pokemons);
   return (
     <ul>
       {pokemons.map((p: Pokemon) => (
---
+++ node/graphql-codegen-transform-lab/src/components/usePokemonsQuery.ts
@@ -0,0 +1,29 @@
+import { useQuery } from "urql";
+import {
+  Pokemon,
+  Pokemons,
+  PokemonsQuery,
+} from "../graphql-client/generated/graphql";
+
+const transform = (
+  pokemons: Array<Pokemon | null> | null | undefined
+): Pokemon[] => {
+  if (!pokemons) {
+    return [];
+  }
+  const newPokemons: Pokemon[] = [];
+  pokemons.forEach((pokemon: Pokemon | null) => {
+    if (pokemon === null) {
+      return;
+    }
+    newPokemons.push(pokemon);
+  });
+  return newPokemons;
+};
+
+export const usePokemonsQuery = () => {
+  const result = useQuery<PokemonsQuery>({ query: Pokemons });
+  const [query] = result;
+  const { data, fetching, error } = query;
+  return { pokemons: transform(data?.pokemons), fetching, error };
+};
