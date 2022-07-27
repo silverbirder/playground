37df3e222e04e7db27bbcf59465b86f4f70addb0
shibamoto <silverbirder@gmail.com>
2022-07-27 15:28:31 +0900

    :recycle: Refactor

--- node/graphql-codegen-transform-lab/src/components/pokemons.tsx
+++ node/graphql-codegen-transform-lab/src/components/pokemons.tsx
@@ -5,24 +5,36 @@ import {
   PokemonsQuery,
 } from "../graphql-client/generated/graphql";
 
+const transform = (
+  pokemons: Array<Pokemon | null> | null | undefined
+): Pokemon[] => {
+  if (!pokemons) {
+    return [];
+  }
+  const newPokemons: Pokemon[] = [];
+  pokemons.forEach((pokemon: Pokemon | null) => {
+    if (pokemon === null) {
+      return;
+    }
+    newPokemons.push(pokemon);
+  });
+  return newPokemons;
+};
+
 const PokemonsComponent = () => {
   const result = useQuery<PokemonsQuery>({ query: Pokemons });
   const [query] = result;
   const { data, fetching, error } = query;
   if (fetching) return <p>Loading...</p>;
   if (error) return <p>Oh no... {error.message}</p>;
+  const pokemons = transform(data?.pokemons);
   return (
     <ul>
-      {data &&
-        data?.pokemons?.map((p: Pokemon | null) => {
-          return (
-            p && (
-              <li key={p.id}>
-                <div>{p.name}</div>
-              </li>
-            )
-          );
-        })}
+      {pokemons.map((p: Pokemon) => (
+        <li key={p.id}>
+          <div>{p.name}</div>
+        </li>
+      ))}
     </ul>
   );
 };
