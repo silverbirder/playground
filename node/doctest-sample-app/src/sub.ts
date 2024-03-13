f04cbd8eb7433ad08842197182dc857f1892fb77
shibamoto <silverbirder@gmail.com>
2024-03-13 20:00:39 +0900

    add sample code of tsdoc-testify

---
+++ node/doctest-sample-app/src/sub.ts
@@ -0,0 +1,29 @@
+/**
+ * sub function
+ *
+ * @remarks
+ * demo
+ *
+ * @example
+ *
+ * ```
+ * import * as assert from "assert";
+ * import { sub } from "./sub";
+ *
+ * assert.equal(sub(2, 1), 1);
+ * ```
+ *
+ * @example
+ *
+ * ```
+ * import * as assert from "assert";
+ * import { sub } from "./sub";
+ *
+ * assert.equal(sub(4, 5), -1);
+ * ```
+ * @param a
+ * @param b
+ */
+export function sub(a: number, b: number) {
+  return a - b;
+}
